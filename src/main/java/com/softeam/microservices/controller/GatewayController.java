17930cb9020ddd3f11d02a46e2d37ebc10d65058
HP5000SERIES\avila <avilameza@gmail.com>
2026-01-07 19:27:38 -0500

    AJUSTES EN GENERALES

---
+++ src/main/java/com/softeam/microservices/controller/GatewayController.java
@@ -0,0 +1,17 @@
+package com.softeam.microservices.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class GatewayController {
+    @GetMapping("/authorized")
+    public Map<String,String> authorized(@RequestParam String code) {
+        return Collections.singletonMap("code: ",code);
+    }
+}
